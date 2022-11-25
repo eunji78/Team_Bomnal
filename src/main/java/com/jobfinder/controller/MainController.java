f4c12323f2ecf1357b5be4fbdf09b990150e1f84
kalfen <kalfen@naver.com>
2022-11-25 14:32:33 +0900

    검색 결과

--- src/main/java/com/jobfinder/controller/MainController.java
+++ src/main/java/com/jobfinder/controller/MainController.java
@@ -5,6 +5,7 @@ import com.jobfinder.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
 public class MainController {
 
     @Autowired
@@ -30,7 +30,12 @@ public class MainController {
     public List<Job> get_job_detail(@RequestParam("super_seq") int super_seq){
 
         List<Job> jobDetail = mainService.getJobDetail(super_seq);
-        System.out.println("jobDetail = " + jobDetail);
         return jobDetail;
     }
+
+    @GetMapping(path = "/list")
+    public String list(){
+        return "searchList";
+    }
+
 }
