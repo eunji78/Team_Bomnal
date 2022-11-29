fe4260cf94179901adb4ddbd2bcc36ceb426e2f2
eun_k_j <eun_k_j@naver.com>
2022-11-29 17:18:16 +0900

    상세페이지 백엔드 작업+메뉴 스크롤 적용

--- src/main/java/com/jobfinder/controller/RecruitController.java
+++ src/main/java/com/jobfinder/controller/RecruitController.java
@@ -1,10 +1,15 @@
 package com.jobfinder.controller;
 
+import com.jobfinder.service.RecruitService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RecruitController {
+
+    @Autowired
+    RecruitService recruitService;
 
     @GetMapping("/noticeDetail")
     public String noticeDetail(){
