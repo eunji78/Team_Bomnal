f5ccabd500cafd97fe07b9d497c5ea99b4a770a4
혜민 <o_o1x_x@naver.com>
2022-12-07 11:16:55 +0900

    front 수정

--- src/main/java/com/jobfinder/domain/Criteria.java
+++ src/main/java/com/jobfinder/domain/Criteria.java
@@ -9,6 +9,7 @@ public class Criteria {
     private int page; // 현재 페이지 번호
     private int perPageNum; // 한 페이지에 보여줄 게시글의 수
     private String industry_class;
+    private String keyword;
     
     public Criteria() {
         this.page = 1; // 현재 페이지 = 1
