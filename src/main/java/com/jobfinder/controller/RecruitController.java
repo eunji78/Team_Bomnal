15025cecfca9f7313e4e01431ce1486db7deed43
박의민 <magicofclown@naver.com>
2022-12-07 20:39:54 +0900

    1. JobPostingController에 있는 썸머노트 관련 코드 RecruitService로 이동
    2. 최근 취업 공고 링크 연결

--- src/main/java/com/jobfinder/controller/RecruitController.java
+++ src/main/java/com/jobfinder/controller/RecruitController.java
@@ -34,27 +34,7 @@ public class RecruitController {
 
     public JsonObject uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile) {
 
-        JsonObject jsonObject = new JsonObject();
-
-        String fileRoot = "C:\\summernote_image\\";	//저장될 외부 파일 경로
-        String originalFileName = multipartFile.getOriginalFilename();	//오리지날 파일명
-        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));	//파일 확장자
-
-        String savedFileName = UUID.randomUUID() + extension;	//저장될 파일 명
-
-        File targetFile = new File(fileRoot + savedFileName);
-
-        try {
-            InputStream fileStream = multipartFile.getInputStream();
-            FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일 저장
-            jsonObject.addProperty("url", "/summernoteImage/"+savedFileName);
-            jsonObject.addProperty("responseCode", "success");
-
-        } catch (IOException e) {
-            FileUtils.deleteQuietly(targetFile);	//저장된 파일 삭제
-            jsonObject.addProperty("responseCode", "error");
-            e.printStackTrace();
-        }
+        JsonObject jsonObject = recruitService.uploadSummernoteImageFile(multipartFile);
 
         return jsonObject;
     }
