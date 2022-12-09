294fce3b4e3d8bb99dc6cd592b20e14382bdc150
eun_k_j <eun_k_j@naver.com>
2022-12-09 15:32:04 +0900

    d-day 수정완료!

--- src/main/java/com/jobfinder/domain/Criteria.java
+++ src/main/java/com/jobfinder/domain/Criteria.java
@@ -10,8 +10,7 @@ public class Criteria {
     private int perPageNum; // 한 페이지에 보여줄 게시글의 수
     private String industry_class;
     private String keyword;
-    private int super_job_seq;
-    
+
     public Criteria() {
         this.page = 1; // 현재 페이지 = 1
         this.perPageNum = 10; // 한 페이지 당 게시글을 10개씩
