492c99d7bd79cf0550e4f30e36d9526869d92caa
kalfen <kalfen@naver.com>
2022-12-09 18:09:30 +0900

    이력서 수정 작업중

--- src/main/java/com/jobfinder/controller/MyPageController.java
+++ src/main/java/com/jobfinder/controller/MyPageController.java
@@ -1,15 +1,32 @@
 package com.jobfinder.controller;
 
+import com.jobfinder.domain.*;
+import com.jobfinder.service.ResumeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
+@RequiredArgsConstructor
 public class MyPageController {
+
+    private final ResumeService resumeService;
 
     @GetMapping("/resume_form")
     public String resume_form(){
         return "resume_form";
     }
+
+    @GetMapping("/resume_edit/{seq}")
+    public String resume_edit(@RequestParam int seq , Resume resume, Awards awards, Language language,
+                              Career career, Certificate certificate, HttpSession session){
+
+        return "resume_edit";
+    }
+
 
     @GetMapping("/resume_manage")
     public String resumeMange(){
